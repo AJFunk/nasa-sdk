257c200d7dfae0b5b83f83b606af495f97e46e1b
Steven Spann <sspann21@gmail.com>
2017-04-09 09:47:03 -0500

    Added epic.natural and epic.enhanced. Still needs work

---
+++ src/epic.js
@@ -0,0 +1,32 @@
+import Q from 'q';
+import { sendRequest } from './util';
+
+export default function epic(): object {
+  return {
+
+    natural(urlParam: string = ''): undefined {
+      const deferred = Q.defer();
+      sendRequest(`https://api.nasa.gov/epic/api/natural/${urlParam}`,
+        {},
+        (err: string, data: object): undefined => {
+          if (err) return deferred.reject(err);
+          return deferred.resolve(data);
+        }
+      );
+      return deferred.promise;
+    },
+
+    enhanced(urlParam: string = ''): undefined {
+      const deferred = Q.defer();
+      sendRequest(`https://api.nasa.gov/epic/api/enhanced/${urlParam}`,
+        {},
+        (err: string, data: object): undefined => {
+          if (err) return deferred.reject(err);
+          return deferred.resolve(data);
+        }
+      );
+      return deferred.promise;
+    },
+
+  };
+}
