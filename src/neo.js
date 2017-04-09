982fbd3178c4125d10a5dc114ad1ae5c0492cfb2
AJ Funk <aj.w.funk.9@gmail.com>
2017-04-09 00:17:38 -0700

    fetch asteroids by ID

--- src/neo.js
+++ src/neo.js
@@ -6,10 +6,21 @@ export default function neo(): object {
 
     feed(options: object = {}): undefined {
       const deferred = Q.defer();
-      // TODO: check for options.start_date since it's required
-      // validate YYYY-MM-DD format for start_date and end_date
       sendRequest('https://api.nasa.gov/neo/rest/v1/feed',
         options,
+        (err: string, data: object): undefined => {
+          if (err) return deferred.reject(err);
+          return deferred.resolve(data);
+        }
+      );
+      return deferred.promise;
+    },
+
+    fetch(asteroidId: string): undefined {
+      const deferred = Q.defer();
+      if (!asteroidId) deferred.reject(new Error('asteroidId is required'));
+      sendRequest(`https://api.nasa.gov/neo/rest/v1/neo/${asteroidId}`,
+        {},
         (err: string, data: object): undefined => {
           if (err) return deferred.reject(err);
           return deferred.resolve(data);
