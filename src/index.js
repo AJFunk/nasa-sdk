c9f70f293a7036f493e71ccbba4f2dcacc69a50e
Steven Spann <sspann21@gmail.com>
2017-04-09 09:53:21 -0500

    Added epic() to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import apod from './apod';
 import neo from './neo';
+import epic from './epic';
 import { setNasaApiKey } from './config';
 
 const APOD = apod();
 const NEO = neo();
+const EPIC = epic();
 
 export {
   APOD,
   NEO,
+  EPIC,
   setNasaApiKey,
 };
