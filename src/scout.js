7f716c273b9f02d0483a99335678d3104e775bf5
AJ Funk <aj.w.funk.9@gmail.com>
2017-04-12 15:50:56 -0700

    fix Scout module

--- src/scout.js
+++ src/scout.js
@@ -1,13 +1,13 @@
 import { sendRequest } from './util';
 
-export default function sentry(): object {
+export default function scout(): object {
   return {
 
     fetch(options: object = {}): undefined {
       return new Promise((resolve: object, reject: object): undefined =>
         sendRequest(
           'ssd-api.jpl.nasa.gov',
-          '/sentry.api',
+          '/scout.api',
           options,
           (err: string, data: object): undefined => {
             if (err) return reject(new Error(err));
