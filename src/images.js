11e0c1b4a045651cb23ed1c177d12b4089664bee
Steven Spann <sspann21@gmail.com>
2017-04-28 18:23:07 -0500

    Feature/images (#19)
    * update README
    
    * Added images module and images test skeleton
    
    * Flagged noApi key prop in images module. Added unit tests. All passing
    
    * Added images module usage instructions to readme
    
    * fix Buffer concat issues
    
    * fix Flow error
    
    * Added images module and images test skeleton
    
    * Flagged noApi key prop in images module. Added unit tests. All passing
    
    * Added images module usage instructions to readme
    
    * Refactored images to match handleResult flow. Made an image error message more verbose
    
    * Added functional validation function for media type. Possible refactor though
    
    * Added images api docs link to README
    
    * Added an easier check for checking options object length
    
    * replaced options length check with something more concise
    
    * Removed console.log()

---
+++ src/images.js
@@ -0,0 +1,92 @@
+// @flow
+import {
+  handleResult,
+  sendRequest,
+} from './util';
+
+export default function images(): Object {
+  const baseurl = 'images-api.nasa.gov';
+
+  function validateMediaType(mediaType: string): boolean {
+    if (!mediaType || typeof mediaType !== 'string') return false;
+    return mediaType.match(/^image$|^audio$|^image,audio$|^audio,image$/) !== null;
+  }
+
+  function validateYear(year: string): boolean {
+    if (!year || typeof year !== 'string') return false;
+    return year.match(/^\d{4}$/) !== null;
+  }
+
+  return {
+
+    search: (options: Object = {}): Promise<any> =>
+      new Promise((resolve: (data: Object) => void, reject: (reason: Error) => void): mixed => {
+        if (!Object.keys(options).length) {
+          return reject(new Error('Atleast one search param is required'));
+        }
+        if (options.hasOwnProperty('media_type') && !validateMediaType(options.media_type)) {
+          return reject(
+            new Error('media_type values must match image||audio||image,audio||audio,image')
+          );
+        }
+        if (options.hasOwnProperty('year_start') && !validateYear(options.year_start)) {
+          return reject(new Error('year_start must be in "YYYY" format'));
+        }
+        if (options.hasOwnProperty('year_end') && !validateYear(options.year_end)) {
+          return reject(new Error('year_end must be in "YYYY" format'));
+        }
+        return sendRequest(
+          baseurl,
+          '/search',
+          options,
+          resolve,
+          reject,
+          handleResult,
+          true
+        );
+      }),
+
+    asset: (nasaId: string): Promise<any> =>
+      new Promise((resolve: (data: Object) => void, reject: (reason: Error) => void): mixed => {
+        if (!nasaId) return reject(new Error('nasaId is required'));
+        return sendRequest(
+          baseurl,
+          `/asset/${nasaId}`,
+          {},
+          resolve,
+          reject,
+          handleResult,
+          true
+        );
+      }),
+
+    metadata: (nasaId: string): Promise<any> =>
+      new Promise((resolve: (data: Object) => void, reject: (reason: Error) => void): mixed => {
+        if (!nasaId) return reject(new Error('nasaId is required'));
+        return sendRequest(
+          baseurl,
+          `/metadata/${nasaId}`,
+          {},
+          resolve,
+          reject,
+          handleResult,
+          true
+        );
+      }),
+
+    captions: (nasaId: string): Promise<any> =>
+      new Promise((resolve: (data: Object) => void, reject: (reason: Error) => void): mixed => {
+        if (!nasaId) return reject(new Error('nasaId is required'));
+        return sendRequest(
+          baseurl,
+          `/captions/${nasaId}`,
+          {},
+          resolve,
+          reject,
+          handleResult,
+          true,
+        );
+      }),
+
+  };
+}
---
+++ tests/images.spec.js
@@ -0,0 +1,68 @@
+import { Images } from 'index';
+
+describe('Images.search()', () => {
+  it('returns object and collection items array', () =>
+    Images.search({ q: 'Apollo 11' }).then(data => {
+      expect(data).to.be.instanceof(Object);
+      expect(data.collection.items).to.be.instanceof(Array);
+    })
+  );
+
+  it('returns object and collection items array', () =>
+    Images.search({
+      q: 'Apollo 11',
+      description: 'moon landing',
+      media_type: 'image',
+    }).then(data => {
+      expect(data).to.be.instanceof(Object);
+      expect(data.collection.items).to.be.instanceof(Array);
+    })
+  );
+
+  it('returns error message', () =>
+    Images.search().catch(err => expect(err).to.be.an('error'))
+  );
+
+  it('returns error message', () =>
+    Images.search({ media_type: 'img,audio' }).catch(err => expect(err).to.be.an('error'))
+  );
+
+  it('returns error message', () =>
+    Images.search({ year_start: '123-456-789' }).catch(err => expect(err).to.be.an('error'))
+  );
+});
+
+describe('Images.asset()', () => {
+  it('returns object and collection items array', () =>
+    Images.asset('as11-40-5874').then(data => {
+      expect(data).to.be.instanceof(Object);
+      expect(data.collection.items).to.be.instanceof(Array);
+    })
+  );
+
+  it('returns error message', () =>
+    Images.asset().catch(err => expect(err).to.be.an('error'))
+  );
+});
+
+describe('Images.metadata()', () => {
+  it('returns object and specific metadata object', () => {
+    const metadataObj = { location: 'https://images-assets.nasa.gov/image/as11-40-5874/metadata.json' };
+    Images.metadata('as11-40-5874').then(data => assert.deepEqual(data, metadataObj));
+  });
+
+  it('returns error message', () =>
+    Images.metadata().catch(err => expect(err).to.be.an('error'))
+  );
+});
+
+describe('Images.captions()', () => {
+  it('returns object and specific captions object', () => {
+    const captionsObj = { location: 'https://images-assets.nasa.gov/video/172_ISS-Slosh/172_ISS-Slosh.srt' };
+    Images.captions('172_ISS-Slosh').then(data => assert.deepEqual(data, captionsObj));
+  });
+
+  it('returns error message', () =>
+    Images.captions().catch(err => expect(err).to.be.an('error'))
+  );
+});
